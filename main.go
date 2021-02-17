4e88261e7b0196f95464a4d1d890c2a536fbd067
JerryPan <jerry.pan.ii@gmail.com>
2021-02-17 23:45:32 +0800

    add import

--- main.go
+++ main.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+    "encoding/base64"
+    //"encoding/hex"
+    "encoding/json"
+
+    "crypto/hmac"
+	"crypto/sha256"
+	//"crypto/md5"
+
+    "sync"
+    "bytes"
+    "log"
+    "strings"
+    "strconv"
+    "fmt"
+    "net"
+    "time"
+    "regexp"
+
+    "io/ioutil"
+    "net/http"
+)
