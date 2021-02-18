db4cf8b20360944a3876f786a5992d24b05eff02
JerryPan <jerry.pan.ii@gmail.com>
2021-02-18 23:57:30 +0800

    add feishu regexp

--- main.go
+++ main.go
@@ -22,3 +22,6 @@ import (
     "io/ioutil"
     "net/http"
 )
+
+const DingdingWebhookPatten string = `^https\:\/\/oapi\.dingtalk\.com\/robot\/send\?access\_token\=([a-z0-9]{64})$`
+const FeishuWebhookPatten string = `^https\:\/\/open\.feishu\.cn\/open\-apis\/bot\/v2\/hook\/([a-z0-9]{8}\-[a-z0-9]{4}\-[a-z0-9]{4}\-[a-z0-9]{4}\-[a-z0-9]{12})$`
